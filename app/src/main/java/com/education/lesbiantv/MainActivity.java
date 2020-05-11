05dc4b0fde8e0345874877ebd1305a14a231e2bf
HOUSSAM BOUKBA <houssam.boukba@alm-solutions.mobi>
2020-05-11 02:05:17 +0000

    #didn't do much.

--- app/src/main/java/com/education/lesbiantv/MainActivity.java
+++ app/src/main/java/com/education/lesbiantv/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,27 +18,41 @@ public class MainActivity extends AppCompatActivity {
 
     private VideoView videoView;
     private ImageButton btnplaypause;
-    private String VideoUrl ="";
+    private String VideoUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=getIntent();
+        Intent intent = getIntent();
         //VideoUrl ="android.resource://" + getPackageName() + "/" + intent.getIntExtra("video", 0);
-        VideoUrl= intent.getStringExtra("video");
-        Log.d("VideoUrl", "??");
+        VideoUrl = intent.getStringExtra("video");
+        Log.d("VideoUrl", VideoUrl);
 
         videoView = (VideoView) findViewById(R.id.video);
         btnplaypause = (ImageButton) findViewById(R.id.play);
-        MediaController mediaController = new MediaController(MainActivity.this);
-        mediaController.setAnchorView(videoView);
 
-        Uri uri = Uri.parse(VideoUrl);
+        final MediaController mediacontroller = new MediaController(this);
+        mediacontroller.setAnchorView(videoView);
 
-        videoView.setMediaController(mediaController);
-        videoView.setVideoPath(VideoUrl);
-        videoView.start();
+
+        videoView.setMediaController(mediacontroller);
+        videoView.setVideoURI(Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+        videoView.requestFocus();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        videoView.setMediaController(mediacontroller);
+                        mediacontroller.setAnchorView(videoView);
+
+                    }
+                });
+            }
+        });
 
     }
 }
